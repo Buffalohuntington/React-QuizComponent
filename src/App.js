5d1643d79e5e8d0adb0354ec7415d7e29e788f4a
Unknown User <ross.buffington@infoworks-tn.com>
2019-02-13 23:12:14 -0600

    Completed the module

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
 import './App.css'
+import Quiz  from './Quiz.js'
+
 
 class App extends Component {
   render() {
-    return (
-      <div />
-    )
+    return <Quiz />  
+    /*
+    // apparently spacing matters
+
+    return <Quiz / > // works
+    return ( < Quiz / > ) // works
+    return 
+        ( <Quiz /> ) // fails
+    return 
+         <Quiz /> // fails
+    */
+    
   }
 }
 
