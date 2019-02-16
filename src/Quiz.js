43fc7195d0e62dc5256f6c0abdf1c4bedfc1fc9f
Unknown User <Buffalohuntington@users.noreply.github.com>
2019-02-15 21:32:05 -0600

    Module6

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,27 +9,22 @@ class Quiz extends Component {
         super(props)
         this.state = { quiz_position: 1 }
     }
+    showNextQuestion() {
+        this.setState((state)=> {
+            return {quiz_position: state.quiz_position + 1}
+        })
+    }
     render() {
         const isQuizEnd = (this.state.quiz_position - 1 === quizData.quiz_questions.length);
         return (
             <div>
 
                 {isQuizEnd ? <QuizEnd /> :
-                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
+                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)}/>}
 
             </div>
         )
-
     }
 }
 
-/**
- * <div className="QuizQuestions">
-        <ol>
-                <li>{quizData.quiz_questions[0].instruction_text}</li>
-                <li>{quizData.quiz_questions[1].instruction_text}</li>
-          
-            </ol>
-        </div>
- */
 export default Quiz;
